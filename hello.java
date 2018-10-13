5ee0b0b84a58bb327d5c89533961677738a2abd9
Sankeerth <saisankeerth.324@gmail.com>
2018-10-14 00:21:27 +0530

    Added java program

---
+++ hello.java
@@ -0,0 +1,12 @@
+class helloworld 
+{ 
+void getclassName() 
+{ 
+System.out.println(this.getClass()); 
+} 
+public static void main(String args[]) 
+{ 
+helloworld cs = new helloworld(); 
+cs.getclassName();
+}
+}
