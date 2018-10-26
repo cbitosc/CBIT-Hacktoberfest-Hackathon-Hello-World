42c66522ddc0bade4a166db961bbfbdf7f6e6165
ryderovsky <44508092+ryderovsky@users.noreply.github.com>
2018-10-26 19:16:18 +0100

    Create file.php

---
+++ Php/file.php
@@ -0,0 +1,8 @@
+<?php
+  $array = array("name"=>"Hacktoberfest","type"=>"Event","colour"=>"purple","manufactured"=>"2018");
+  $array2 = array("Hacktoberfest","Event","purple","2018");
+  $array3 = array("name"=>"Hacktoberfest","Event","colour"=>"purple","2018");
+  print_r($array);
+  print_r($array2);
+  print_r($array3);
+?>
